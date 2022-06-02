228e4a75c640913c6b110522086dba4302d14579
kalab-scorpio <kalabkassa0@gmail.com>
2022-06-02 11:07:31 +0300

    first c code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include<stdio.h>
 
-int main(void){
+/* * 
+ * this is the entery or main functions 
+ */
+
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
