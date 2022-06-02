102da6ce5f1b799fd45be5391e8ede0fc7323022
kalab-scorpio <kalabkassa0@gmail.com>
2022-06-02 11:22:21 +0300

    printf in c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+/**
+ * main - just prints "with proper grammar, but the outocme is a piece of art"
+ * Return: 0 if all goes well
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
