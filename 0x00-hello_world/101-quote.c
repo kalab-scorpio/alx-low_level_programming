79dcd515a9958e35eb165822e23264ac2653247f
kalab-scorpio <kalabkassa0@gmail.com>
2022-06-02 12:01:42 +0300

    fwrite

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+/**
+ * main - print str to stderr
+ * Return: 1 if all goes well
+ */
+
+int main(void)
+{
+	static const char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	fwrite(str, sizeof(str) - 1, 1, stderr);
+}
