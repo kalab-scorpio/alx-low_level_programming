d7ff28d890541b46bf4d67d8f6e25d6792105d81
kalab-scorpio <kalabkassa0@gmail.com>
2022-06-04 00:23:44 +0300

    p or n

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+
+/**
+ * main - if the random generated number is weather negative, positive or zero
+ * Return: 0 if all goes well
+ */
+
+int main(void)
+{
+	int n, m;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	m = n % 10;
+	if (n > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, m);
+	else if (n == 0)
+		printf("Last digit of %d is %d and is 0\n", n, m);
+	else if (n < 6)
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n);
+	return (0);
+}
