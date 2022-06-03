0a03034090ee5fc14803bc95c37978d46dfdb2ac
kalab-scorpio <kalabkassa0@gmail.com>
2022-06-04 01:06:31 +0300

    0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print the numbers 0-9
+ * Return: 0 if all goes well
+ */
+
+int main(void)
+{
+	int x = 0;
+
+	while(x < 10)
+		printf("%d", x++);
+	printf("\n");
+	return (0);
+}
