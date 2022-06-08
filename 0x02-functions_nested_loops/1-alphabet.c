24b020ceceac2870a2495c5b7903024eb89005aa
kalab-scorpio <kalabkassa0@gmail.com>
2022-06-08 11:17:51 +0300

    alphabet lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include"main.h"
+
+/**
+ * -print alphabet in lowercase followed by a new line
+ *  Retutrn: 0 if it works correctly
+ */
+
+void print_alphabet(void)
+{
+	for (i = 97; i <= 122; i++)
+		_putchar(i);
+
+	_putchar(\n);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,5 @@
 #ifndef HEADER
 #define HEADER
 int _putchar(char c);
+void print_alphabet(void);
 #endif
