af14e7592e2d6782d55f1254b0e6f301dc8c8295
kalab-scorpio <kalabkassa0@gmail.com>
2022-06-08 18:53:42 +0300

    alphabet

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,7 +2,7 @@
 
 /**
  * _isalpha - print 1 if it is a letter and 0 otherwise
- * @c: variable for accepting char 
+ * @c: variable for accepting char
  * Return: 0 or 1 if it works correctly
  */
 
