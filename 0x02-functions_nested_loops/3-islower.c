cf0d54409c92b89c0fd8d5c919c3e2318decbb8b
kalab-scorpio <kalabkassa0@gmail.com>
2022-06-08 17:59:40 +0300

    lower case

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,7 +3,7 @@
 /**
  * _islower - returns 1 if it is lowercase letter otherwise 0
  * @c: the char decimal value to be evaluated
- * Retutrn: 0 or 1 if it works correctly
+ * Return: 0 or 1 if it works correctly
  */
 
 int _islower(int c)
