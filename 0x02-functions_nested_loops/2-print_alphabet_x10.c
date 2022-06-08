5babaff101e88d366608280d468c133251f390c8
kalab-scorpio <kalabkassa0@gmail.com>
2022-06-08 17:31:54 +0300

    alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,19 @@
 #include"main.h"
 
 /**
- * print_alphabet - print alphabet in lowercase followed by a new line
+ * print_alphabet_x10 - print alphabet in lowercase followed by a new line
  * Retutrn: 0 if it works correctly
  */
 
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 	int i, j;
-	
-	for(j = 0; j<= 10; j++) {
+
+	for (j = 0; j <= 10; j++)
+	{
 		for (i = 97; i <= 122; i++)
 			_putchar(i);
 		_putchar('\n');
 	}
+	return (0);
 }
