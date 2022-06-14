6a9480a2ff6db2cbdd1ac2a28eda295ae3aae08a
kalab-scorpio <kalabkassa0@gmail.com>
2022-06-14 14:38:00 +0300

    print reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+/**
+ * print_rev - print the array of char in reverse
+ * @s: the array of char to be printed
+ */
+#include<string.h>
+#include"main.h"
+
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = strlen(s); i > 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
