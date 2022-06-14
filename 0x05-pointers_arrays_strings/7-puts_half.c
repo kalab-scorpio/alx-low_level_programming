e05a325ecf0c47fe6489257be7148a0d75f6d86c
kalab-scorpio <kalabkassa0@gmail.com>
2022-06-14 20:28:08 +0300

    print second half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,15 @@
+/**
+ * puts_half - print's second half of str
+ * @str: char array to be printed
+ */
+#include<string.h>
+#include "main.h"
+
+void puts_half(char *str)
+{
+	int i = (int)strlen(str)/2;
+
+	for(; i < (int)strlen(str); i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
