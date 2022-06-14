80ed5348b6c5ab11e0a27b7f9772cf0be49b4345
kalab-scorpio <kalabkassa0@gmail.com>
2022-06-14 14:23:25 +0300

    strlen

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,3 +1,11 @@
 /**
  * _puts - prints arrays
- * @
+ * @str: the array of chars
+ */
+#include<string>
+
+void _puts(char *str)
+{
+	for (int i = 0; i < strlen(str); i++)
+		_putchar(putstr[0]);
+}
