c779137e5492014f385d737a894a07cd20ab8cc9
kalab-scorpio <kalabkassa0@gmail.com>
2022-06-14 20:53:10 +0300

    print second half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,8 +9,8 @@ void puts_half(char *str)
 {
 	int i = (int)strlen(str)/2;
 
-	if ((int)strlen(str) % 2 != 0)
-		i--;
+	if ((int)strlen(str) % 2 == 1)
+		i++;
 	for (; i < (int)strlen(str); i++)
 		_putchar(str[i]);
 	_putchar('\n');
