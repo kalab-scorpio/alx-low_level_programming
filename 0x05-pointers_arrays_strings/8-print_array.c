e34b72ae1fca24146e675417120beb5398b83cb9
kalab-scorpio <kalabkassa0@gmail.com>
2022-06-14 21:11:40 +0300

    print array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+/**
+ * print_array - function to print array
+ * @n: size of the array to be printed
+ * @a: int array
+ */
+#include<stdio.h>
+
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	for(; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if(i != n)
+			printf(",");
+	}
+}
