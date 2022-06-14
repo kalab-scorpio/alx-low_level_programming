28fa0edc3150b223e84713d55884dd8876932c6e
kalab-scorpio <kalabkassa0@gmail.com>
2022-06-14 20:08:05 +0300

    print reverse

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+/**
+ * puts2 - function that prints str jumping one at a time
+ * @str: char array to be printed
+ */
+#include<string.h>
+#include "main.h"
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	for (; i < (int)strlen(str); i++)
+	{
+		if (i%2 = 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
