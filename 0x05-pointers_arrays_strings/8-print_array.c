c1347bea1f3844a33dbb5bfd5f859e2b4ce3982d
kalab-scorpio <kalabkassa0@gmail.com>
2022-06-14 21:27:37 +0300

    print array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,10 +9,10 @@ void print_array(int *a, int n)
 {
 	int i = 0;
 
-	for(; i < n; i++)
+	for (; i < n; i++)
 	{
 		printf("%d", a[i]);
-		if(i != n - 1)
+		if (i != n - 1)
 			printf(", ");
 	}
 	printf("\n");
