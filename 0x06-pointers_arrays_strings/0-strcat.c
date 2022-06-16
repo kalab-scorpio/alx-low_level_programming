87785b8c34f5d3c0de3936bb6d50d2b11f960a39
kalab-scorpio <kalabkassa0@gmail.com>
2022-06-16 19:50:45 +0300

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,11 @@
+/**
+ * _strcat - string concatination
+ * @dest: array of string for concatination
+ * @src: source of string for concatination
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char *result = dest + src;
+	return result;
+}
