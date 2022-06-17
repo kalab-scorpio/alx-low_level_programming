2ea35216a31a2dd0c7c1b8a1811b23156e3a4905
kalab-scorpio <kalabkassa0@gmail.com>
2022-06-17 15:36:09 +0300

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,12 @@
+/**
+ * _strcmp - function to compare two char arrays
+ * @s1: first char array to be compared
+ * @s2: second char array to be compared
+ * Return: returns int
+ */
+#include<string.h>
+
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
