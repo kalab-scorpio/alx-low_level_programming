dba39dc40ec8b8dfc31b9c4b73559b3eaba2c066
kalab-scorpio <kalabkassa0@gmail.com>
2022-06-17 15:44:03 +0300

    rev_array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,14 @@
+/**
+ * reverse-array - functions that reverses teh content of an array of integers
+ * @a: the array of integerst to be reversed
+ * @n: the number of element of array
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i = 0;
+	int b[] = a;
+
+	for(; i < n; i++)
+		a[i] = b[n-i-1];
+}
