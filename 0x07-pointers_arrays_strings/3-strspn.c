0437f9ef5b290646fc74ecaa9351e6ed3029ff44
kalab-scorpio <kalabkassa0@gmail.com>
2022-06-20 21:30:18 +0300

    strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,12 +1,12 @@
 /**
  * _strspn - returns the number of byte that accept contained in s
  * @s: main string
- * @accept: string 
+ * @accept: string
  * Return: int
  */
 #include <string.h>
 
 unsigned int _strspn(char *s, char *accept)
 {
-	return strspn(s, accept);
+	return (strspn(s, accept));
 }
