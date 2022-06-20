00f3cd2524dc263983bfe8747a3f1cf3e424fb19
kalab-scorpio <kalabkassa0@gmail.com>
2022-06-20 21:49:33 +0300

    strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,12 @@
+/**
+ * _strpbrk - locates teh first occurrence in s of any of the bytes in accept
+ * @s: string s
+ * @accept: str to check if s contains accept
+ * Return: returns pointer of s 
+ */
+#include <string.h>
+
+char *_strpbrk(char *s, char *accept)
+{
+	return (strpbrk(s, accept));
+}
