c191609c52e8fb7bb883012f095ba6a8209585f5
kalab-scorpio <kalabkassa0@gmail.com>
2022-06-20 20:42:48 +0300

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,7 +3,7 @@
  * @s: a char pointer to be filled by const variable b
  * @b: char data to be filled to s
  * @n:  the first bytes of memory area of s
- * Retrun: a pointer of s
+ * Return: a pointer of s
  */
 #include <string.h>
 
