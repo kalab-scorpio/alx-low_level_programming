be169399d94fca59976a2b3f205dbcd8baf23c04
kalab-scorpio <kalabkassa0@gmail.com>
2022-06-21 16:52:56 +0300

    add the diagonals

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,23 @@
+/**
+ * print_diagsums - prints the sum of the two diagonals of a matrix
+ * @a: the array that holds the square matrix
+ * @size: the size of the square matrix
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int sum = 0, sum2 = 0, row, col;
+
+	for (row = 0; row < size; row++)
+	{
+		for (col = 0; col < size; col++) 
+		{
+			if (row == col)
+				sum += a[row][col];
+			else if (row + col = size)
+				sum2 += a[row][col];
+		}
+	}
+	printf("%d, %d", sum, sum2)
+}
+
