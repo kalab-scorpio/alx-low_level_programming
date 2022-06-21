6ab19299615816ee5d402eee6a7420bfa80e56ef
kalab-scorpio <kalabkassa0@gmail.com>
2022-06-21 15:46:13 +0300

    chess board

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -6,8 +6,10 @@
 
 void print_chessboard(char (*a)[8])
 {
-	for (int row = 0; row < 8; row++) {
-            for (int col = 0; col < 8; col++)
+	int row, col;
+
+	for (row = 0; row < 8; row++) {
+            for (col = 0; col < 8; col++)
                 _putchar(a[row][col]);
 	    _putchar('\n');
 	}
