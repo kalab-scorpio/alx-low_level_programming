e65beccf3f1598f52ace0d5a567075b40f17597f
kalab-scorpio <kalabkassa0@gmail.com>
2022-06-22 22:11:10 +0300

    return the size

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -6,14 +6,13 @@
 
 int factorial(int n)
 {
+	int f = factorial(n-1);
+
 	if (n < 0)
 		return (-1);
 
-	int f;
-
-	f = factorial(n - 1);
-
-	if (f >= 0)
+	else if (f >= 0)
 		return (n * f);
+
 	return (1);
 }
