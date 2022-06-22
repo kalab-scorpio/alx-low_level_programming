720b3e21f52fb0bb3da7ed64cbb0c12219627006
kalab-scorpio <kalabkassa0@gmail.com>
2022-06-22 22:23:04 +0300

    factorial

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,14 @@
+/**
+ * _pow_recursion - x**y using recursion
+ * @x: the var to be raised to the power of y
+ * @y: the var that holds the power
+ * Return: x**y
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if ( y == 0)
+		return 1;
+	else	
+		return (x * _pow_recursion(x, y - 1));
+}	
