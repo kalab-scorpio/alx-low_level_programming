bed3f80b404185f88796e6f3d24c76368e4e8792
kalab-scorpio <kalabkassa0@gmail.com>
2022-06-22 20:36:17 +0300

    puts with recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,10 +6,12 @@
 
 void _puts_recursion(char *s)
 {
-	int i = 0;
+	static int i = 0;
 
-	while(s[i] != '\0'){
-		_putchar(s[i]);
-		i++;
-	}
+	if(s[i] == '\0')
+		return;
+	_putchar(s[i]);
+	i++;
+
+	_puts_recursion(s);
 }
