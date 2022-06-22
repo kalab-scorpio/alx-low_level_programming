f6620de86ba118eb05c2fbc75a0fd84431847223
kalab-scorpio <kalabkassa0@gmail.com>
2022-06-22 21:39:35 +0300

    return the size

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,11 +1,11 @@
 /**
  * _strlen_recursion - returns the length of string
- * @s: char array 
+ * @s: char array
  * Return: returns the length of s
  */
 #include <string.h>
 
-int _strlen_recursion(char *s) 
+int _strlen_recursion(char *s)
 {
 	return (strlen(s));
 }
