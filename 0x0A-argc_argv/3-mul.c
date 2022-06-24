69190b027d69369ca90b5aafd55f4cefb50f71a5
kalab-scorpio <kalabkassa0@gmail.com>
2022-06-24 14:14:03 +0300

    multriply

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+/**
+ * main - prints it's name
+ * @argc: number of arguments
+ * @argv: array for holding the arguments
+ * Return: 0 if all goes well
+ */
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	if (argc < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		int i = atoi(argv[1]);
+		int j = atoi(argv[2]);
+		printf("%d\n", i * j);
+	}
+	return (0);
+}
