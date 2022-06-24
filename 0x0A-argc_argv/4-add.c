373184cb48888d0531b93220b9e5b4d45fb22780
kalab-scorpio <kalabkassa0@gmail.com>
2022-06-24 15:52:16 +0300

    adding

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,23 +11,28 @@ int main(int argc, char *argv[])
 {
 	int i = 1, sum = 0;
 
-	if (argc == 1)
+	if (argc > 1)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	for (; i < argc; i++)
+	else if (argc == 1)
+		printf("%d\n", 0);
+	else
 	{
-		int j = atoi(argv[i]);
-
-		if (atoi(argv[i]) && j >= 0)
-			sum += j;
-		else
+		for (; i < argc; i++)
 		{
-			printf("Error\n");
-			return (1);
+			int j = atoi(argv[i]);
+
+			if (atoi(argv[i]) && j >= 0)
+				sum += j;
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
+		printf("%d\n", sum);
 	}
-	printf("%d\n", sum);
 	return (0);
 }
