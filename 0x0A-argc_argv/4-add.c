1c3d05d591821fd7934b630a37071c3c8d01b093
kalab-scorpio <kalabkassa0@gmail.com>
2022-06-24 15:56:51 +0300

    adding

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,12 +11,7 @@ int main(int argc, char *argv[])
 {
 	int i = 1, sum = 0;
 
-	if (argc > 1)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	else if (argc == 1)
+	if (argc == 1)
 		printf("%d\n", 0);
 	else
 	{
