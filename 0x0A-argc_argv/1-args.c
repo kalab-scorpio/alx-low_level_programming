dc22da7a77788c4f8221e4a30bdcc66e29119a85
kalab-scorpio <kalabkassa0@gmail.com>
2022-06-24 13:47:08 +0300

    number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+/**
+ * main - prints the nubmer of arguments
+ * @argc: number of arguments
+ * @argv: array for holding the arguments
+ * Return: 0 if all goes well
+ */
+#include<stdio.h>
+
+int main(int argc, char* argv[] __attribute__((unused)))
+{
+
+	printf("%d\n", argc - 1);
+	return (0);
+}
