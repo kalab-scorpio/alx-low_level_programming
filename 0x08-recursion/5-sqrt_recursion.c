1b43a9fecdc3945cb90fef15176d6bb9119ef762
kalab-scorpio <kalabkassa0@gmail.com>
2022-06-24 13:25:35 +0300

    what is my name

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,10 @@
+/**
+ * _sqrt_recursion - calculates square root using recursion
+ * @n: number to be calculated
+ * Return: the square root value of n
+ */
+
+int _sqrt_recursion(int n)
+{
+	return _sqrt_recursion(n - 1);
+}
