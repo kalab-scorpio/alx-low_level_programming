120e0ac9d519f46ae92a98ddb5eb8cb49801a2f6
kalab-scorpio <kalabkassa0@gmail.com>
2022-06-24 13:23:16 +0300

    what is my name

---
+++ 0x0A-arc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+/**
+ * main - prints it's name
+ * @argc - number of arguments 
+ * @argv - array for holding the arguments
+ * Return - 0 if all goes well
+ */
+#include<stdio.h>
+
+int main(int argc, char *argv[])
+{
+	printf("%c", argv[0]);
+	return (0);
+}
