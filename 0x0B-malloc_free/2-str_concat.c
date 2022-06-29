c590cd917dda2f0c783afea39e07004002e9f4a4
kalab-scorpio <kalabkassa0@gmail.com>
2022-06-29 16:19:01 +0300

    concatination

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,25 @@
+#include<string.h>
+#include<stdlib.h>
+
+char *str_concat(char *s1, char *s2)
+{
+	char *buffer;
+	unsigned long int i = 0;
+
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+
+	buffer = malloc(strlen(s1) + strlen(s2) + 1);
+
+	if (buffer == NULL)
+		return (NULL);
+
+	for (; i < (strlen(s1) + strlen(s2)); i++)
+	{
+		if (i < strlen(s1))
+			buffer[i] = s1[i];
+		else 
+			buffer[i] = s2[i];
+	}
+	return (buffer);
+}
