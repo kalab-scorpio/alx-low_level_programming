8815831e1fa1e7b03c3185d32b2398f3cf44dc26
kalab-scorpio <kalabkassa0@gmail.com>
2022-06-29 15:46:55 +0300

    strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,7 +11,7 @@ char *_strdup(char *str)
 	char *buffer = malloc(strlen(str) + 1);
 	long unsigned int i = 0;	
 
-	if (str == NULL || buffer == NULL)
+	if (*str == NULL || buffer == NULL)
 		return (NULL);
 	for (; i < strlen(str); i++)
 		buffer[i] = str[i];
