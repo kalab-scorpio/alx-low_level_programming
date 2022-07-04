2e207f656e47b4ac18beacb8e2e865d6a126e897
kalab-scorpio <kalabkassa0@gmail.com>
2022-07-04 22:01:15 +0300

    dog

---
+++ 0x0D-preprocessor/0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * struct dog - holds data about dogs
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the owner of the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
