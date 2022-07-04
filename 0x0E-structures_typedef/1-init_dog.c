b79b6c2b52fd8bf421434c02a372327467e35f4c
kalab-scorpio <kalabkassa0@gmail.com>
2022-07-04 22:10:49 +0300

    init dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,14 @@
+/**
+ * init_dog - initializes the structure dog
+ * @d: the data struct that holdes info about the dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
