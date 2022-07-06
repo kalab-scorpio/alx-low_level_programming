e4818a186e6543b19c86ddea1224a0132587df7e
kalab-scorpio <kalabkassa0@gmail.com>
2022-07-06 19:49:28 +0300

    update

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,7 +1,7 @@
 /**
  * print_name - function that prints name using another function
  * @name: name to be printed
- * *f: variable that pointes to the function that prints the name
+ * @f: variable that pointes to the function that prints the name
  */
 
 #include "function_pointers.h"
