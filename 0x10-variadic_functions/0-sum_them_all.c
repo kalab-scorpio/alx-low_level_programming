dceee7301c19002274e621784b6506729e3332e1
kalab-scorpio <kalabkassa0@gmail.com>
2022-07-07 20:23:50 +0300

    variadic functions

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -18,7 +18,7 @@ int sum_them_all(const unsigned int n, ...)
 	va_start(args, n);
 
 	for (i = 0; i < n; i++)
-		sum += va_arg(valist, const unsigned int);
+		sum += va_arg(args, const unsigned int);
 	va_end(args);
 
 	return (sum);
